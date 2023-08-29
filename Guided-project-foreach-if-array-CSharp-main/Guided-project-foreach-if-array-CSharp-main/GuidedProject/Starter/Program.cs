ff130bf011a9b6ab39b959dd9476b9e1463acf3f
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-08-30 02:15:56 +0800

    Initial commit

---
+++ Guided-project-foreach-if-array-CSharp-main/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int[] sophiaScores = { 90, 86, 87, 98, 100 };
+int[] andrewScores = { 92, 89, 81, 96, 90 };
+int[] emmacores = { 90, 85, 87, 98, 68 };
+int[] loganScores = { 90, 95, 87, 88, 96 };
+
+int sophiaSum = 0;
+int andrewSum = 0;
+int emmaSum = 0;
+int loganSum = 0;
+
+foreach (int i in sophiaScores) 
+{
+    sophiaSum += i;
+}
+
+foreach (int i in andrewScores) 
+{
+    andrewSum += i;
+}
+
+foreach (int i in emmacores) 
+{
+    emmaSum += i;
+}
+
+foreach (int i in loganScores) 
+{
+    loganSum += i;
+}
+
+decimal sophiaScore;
+decimal andrewScore;
+decimal emmaScore;
+decimal loganScore;
+
+sophiaScore = (decimal)sophiaSum / currentAssignments;
+andrewScore = (decimal)andrewSum / currentAssignments;
+emmaScore = (decimal)emmaSum / currentAssignments;
+loganScore = (decimal)loganSum / currentAssignments;
+
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
