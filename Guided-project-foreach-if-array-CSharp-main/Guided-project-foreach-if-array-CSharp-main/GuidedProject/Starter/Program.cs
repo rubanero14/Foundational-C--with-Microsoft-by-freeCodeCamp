aba68aa82cd41886c903a27b2de376351f4f2e44
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-08-30 13:59:26 +0800

    Exercise - Implement code branches using selection statements

--- Guided-project-foreach-if-array-CSharp-main/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -1,53 +1,106 @@
-﻿using System;
-
-// initialize variables - graded assignments 
+﻿// initialize variables - graded assignments 
 int currentAssignments = 5;
 
-int[] sophiaScores = { 90, 86, 87, 98, 100 };
-int[] andrewScores = { 92, 89, 81, 96, 90 };
-int[] emmacores = { 90, 85, 87, 98, 68 };
-int[] loganScores = { 90, 95, 87, 88, 96 };
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
-foreach (int i in sophiaScores) 
-{
-    sophiaSum += i;
-}
+int[] studentScores = new int[10];
 
-foreach (int i in andrewScores) 
-{
-    andrewSum += i;
-}
-
-foreach (int i in emmacores) 
-{
-    emmaSum += i;
-}
-
-foreach (int i in loganScores) 
-{
-    loganSum += i;
-}
-
-decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
-
+// Write the Report Header to the console
 Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        // add the exam score to the sum
+        sumAssignmentScores += score;
+    }
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+
+    string currentStudentLetterGrade = "";
+
+    if (currentStudentGrade >= 97 && currentStudentGrade <= 100)
+    {
+        currentStudentLetterGrade = "A+";
+    }
+    else if (currentStudentGrade >= 93 && currentStudentGrade <= 96) 
+    {
+        currentStudentLetterGrade = "A";
+    }
+    else if (currentStudentGrade >= 90 && currentStudentGrade < 93) 
+    {
+        currentStudentLetterGrade = "A-";
+    }
+    else if (currentStudentGrade >= 87 && currentStudentGrade < 90) 
+    {
+        currentStudentLetterGrade = "B+";
+    }          
+    else if (currentStudentGrade >= 83 && currentStudentGrade < 87) 
+    {
+        currentStudentLetterGrade = "B";
+    }
+    else if (currentStudentGrade >= 80 && currentStudentGrade < 83) 
+    {
+        currentStudentLetterGrade = "B-";
+    }
+    else if (currentStudentGrade >= 77 && currentStudentGrade < 80) 
+    {
+        currentStudentLetterGrade = "C+";
+    }
+    else if (currentStudentGrade >= 73 && currentStudentGrade < 77) 
+    {
+        currentStudentLetterGrade = "C";
+    }
+    else if (currentStudentGrade >= 70 && currentStudentGrade < 73) 
+    {
+        currentStudentLetterGrade = "C-";
+    }
+    else if (currentStudentGrade >= 67 && currentStudentGrade < 70) 
+    {
+        currentStudentLetterGrade = "D+";
+    }
+    else if (currentStudentGrade >= 63 && currentStudentGrade < 67) 
+    {
+        currentStudentLetterGrade = "D";
+    }
+    else if (currentStudentGrade >= 60 && currentStudentGrade < 63) 
+    {
+        currentStudentLetterGrade = "D-";
+    }
+    else if (currentStudentGrade >= 0 && currentStudentGrade < 60)
+    {
+        currentStudentLetterGrade = "F";
+    }
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+}
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
