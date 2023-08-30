c31a4bd60315210eaf28c4044dc1d9a9c0eda2af
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-08-30 17:00:39 +0800

    Exercise - Implement code branches using selection statements

--- Guided-project-foreach-if-array-CSharp-main/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -1,10 +1,10 @@
 ﻿// initialize variables - graded assignments 
 int currentAssignments = 5;
 
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
 // Student names
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
@@ -17,7 +17,7 @@ Console.WriteLine("Student\t\tGrade\n");
 foreach (string name in studentNames)
 {
     string currentStudent = name;
-
+    
     if (currentStudent == "Sophia")
         studentScores = sophiaScores;
 
@@ -36,10 +36,20 @@ foreach (string name in studentNames)
     // initialize/reset the calculated average of exam + extra credit scores
     decimal currentStudentGrade = 0;
 
+    int courseCount = 0;
+
     foreach (int score in studentScores)
     {
-        // add the exam score to the sum
-        sumAssignmentScores += score;
+        courseCount++;
+        if(courseCount > 5) 
+        {
+            sumAssignmentScores += score / 10;
+        }
+        else 
+        {
+            // add the exam score to the sum
+            sumAssignmentScores += score;
+        }
     }
 
     currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
