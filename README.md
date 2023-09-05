053d27b46b7fffb843714bc9fcde5a6cea9c0785
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-05 18:10:53 +0800

    Review the solution to extract, replace, and remove data from an input string

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Foundational-C-with-Microsoft-by-freeCodeCamp
+# Foundational-C#-with-Microsoft-by-freeCodeCamp
+
+## To create new console application:
+
+```
+dotnet new console -o ./{App-Name}/
+```
+
+## To run console application:
+
+```
+dotnet run
+```
+
+## To build console application:
+
+```
+dotnet build
+```
---
+++ TestProject4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("===========================================================");
+
+// Exercise: Complete a challenge to extract, replace, and remove data from an input string
+// Extract, replace, and remove data from an input string
+const string input = $@"
+    <div>
+        <h2>Widgets &trade;</h2>
+        <span>5000</span>
+    </div>
+";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+quantity = input.Remove(0, 61);
+quantity = quantity.Remove(4);
+
+output = input.Remove(45).Replace("\n", "").Replace(" ", "").Replace("<div>", "").Replace("&trade;", " &reg;");
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
+
+Console.WriteLine("===========================================================");
