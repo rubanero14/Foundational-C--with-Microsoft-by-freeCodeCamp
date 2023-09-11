2b2a695af0f5000685e831613c46580060b7f923
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-11 19:25:53 +0800

    Review the solution to create a reusable method

---
+++ Exercise-Complete-the-challenge-to-create-a-reusable-method/Program.cs
@@ -0,0 +1,25 @@
+﻿// Exercise - Complete the challenge to create a reusable method
+
+// Code challenge: create a reusable method
+
+fortuneTeller();
+fortuneTeller();
+fortuneTeller();
+
+void fortuneTeller()
+{
+    Random random = new Random();
+    string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
+    string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
+    string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
+    string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
+    int luck = random.Next(100);
+
+    Console.WriteLine("A fortune teller whispers the following words:");
+    string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+    for (int i = 0; i < 4; i++)
+    {
+        Console.Write($"{text[i]} {fortune[i]} ");
+    }
+    Console.WriteLine("\n");
+}
