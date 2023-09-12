ed8f3131600f446febbfef842e402e8fc408a9bd
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-12 13:32:54 +0800

    Exercise - Return arrays from methods

--- Create-C#-methods-that-return-values/Program.cs
+++ Create-C#-methods-that-return-values/Program.cs
@@ -130,3 +130,82 @@ bool IsPalindrome(string word)
     return word == reversedWord;
 }
 Console.WriteLine("===========================================================");
+// Exercise - Return arrays from methods
+// Find coins to make change
+int[] TwoCoins(int[] coins, int target)
+{
+    for (int curr = 0; curr < coins.Length; curr++)
+    {
+        for (int next = curr + 1; next < coins.Length; next++)
+        {
+            if (coins[curr] + coins[next] == target)
+            {
+                return new int[] { curr, next };
+            }
+
+        }
+    }
+    return new int[0];
+}
+
+// Test your solution
+int target = 60;
+int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[] result = TwoCoins(coins, target);
+
+if (result.Length == 0)
+{
+    Console.WriteLine("No two coins make change");
+}
+else
+{
+    Console.WriteLine($"Change found at positions {result[0]} and {result[1]}");
+}
+Console.WriteLine("===========================================================");
+// Find multiple pairs of coins that make change
+int[,] Two_Coins(int[] coins, int target)
+{
+    int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+    int count = 0;
+
+    for (int curr = 0; curr < coins.Length; curr++)
+    {
+        for (int next = curr + 1; next < coins.Length; next++)
+        {
+            if (coins[curr] + coins[next] == target)
+            {
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
+            }
+            if (count == result.GetLength(0))
+            {
+                return result;
+            }
+        }
+    }
+    return (count == 0) ? new int[0, 0] : result;
+}
+
+// Capture the new return array
+int Target = 30;
+int[] Coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[,] Result = Two_Coins(Coins, Target);
+
+if (Result.Length == 0)
+{
+    Console.WriteLine("No two coins make change");
+}
+else
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < Result.GetLength(0); i++)
+    {
+        if (Result[i, 0] == -1)
+        {
+            break;
+        }
+        Console.WriteLine($"{Result[i, 0]},{Result[i, 1]}");
+    }
+}
+Console.WriteLine("===========================================================");
