e62bdb23ada2de79b1b8122d6cbb9304da05fefa
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-15 02:59:50 +0800

    Debug 101

---
+++ debug-101/Program.cs
@@ -0,0 +1,39 @@
+﻿/* 
+This code uses a names array and corresponding methods to display
+greeting messages
+*/
+
+string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+string messageText = "";
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(i);
+}
+
+foreach (string name in names)
+{
+    if (name == "Sophia")
+        messageText = SophiaMessage();
+    else if (name == "Andrew")
+        messageText = AndrewMessage();
+    else if (name == "AllGreetings")
+        messageText = SophiaMessage();
+    messageText = messageText + "\n\r" + AndrewMessage();
+
+    Console.WriteLine(messageText + "\n\r");
+}
+
+bool pauseCode = true;
+while (pauseCode == true) ;
+
+static string SophiaMessage()
+{
+    return "Hello, my name is Sophia.";
+}
+
+static string AndrewMessage()
+{
+    return "Hi, my name is Andrew. Good to meet you.";
+}
