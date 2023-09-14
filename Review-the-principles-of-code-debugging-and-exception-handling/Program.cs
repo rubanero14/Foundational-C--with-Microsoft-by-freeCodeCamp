77d024a406641260a6d63c0da98f07370162270d
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-14 13:08:47 +0800

    Get started with testing, debugging, and exception handling

--- Review-the-principles-of-code-debugging-and-exception-handling/Program.cs
+++ Review-the-principles-of-code-debugging-and-exception-handling/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("====================================================");
+// Get started with testing, debugging, and exception handling
+// Code debugging and developer responsibilities
+string[] students = new string[] { "Sophia", "Nicolas", "Zahirah", "Jeong" };
+
+int studentCount = students.Length - 1;
+
+Console.WriteLine("The final name is: " + students[studentCount]);
+Console.WriteLine("====================================================");
